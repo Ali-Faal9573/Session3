0333a01f5ebbc304c316c311ed2997c8a2cb30ce
Ali Faal <faal.ali.faal@gmail.com>
2023-09-12 16:12:06 +0330

    documentation app using json files

---
+++ JsonKeywordDocumentationApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using JsonKeywordDocumentationLibrary;
+
+namespace JsonKeywordDocumentationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("C# Modifier Keyword Documentation");
+            Console.ResetColor();
+
+            bool isValid = true;
+
+            while (isValid)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(KeyWordList.showstartingtext());
+                Console.ResetColor();
+                string keyword = Console.ReadLine().ToLower();
+                if (keyword == "exit")
+                {
+                    isValid = false;
+                    break;
+                }
+                string keywordinfo = SelectKeyword.keywordinfo(keyword);
+                if (keywordinfo != null)
+                {
+                    ReadAndConvertFile file = new ReadAndConvertFile(keywordinfo);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(file.convert().keyword);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(file.convert().description);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Beep();
+                    Console.WriteLine("Keyword not recognized.");
+                    Console.ResetColor();
+                }
+
+            }
+        }
+    }
+}
---
+++ JsonKeywordDocumentationLibrary/SelectKeyword.cs
@@ -0,0 +1,62 @@
+﻿using JsonKeywordDocumentationApp;
+
+namespace JsonKeywordDocumentationLibrary
+{
+    public static class SelectKeyword
+    {
+
+        public static string keywordinfo(string keyword)
+        {
+            string projectPath = ProjectPath.GetPath();
+
+
+            switch (keyword.ToLower())
+            {
+                case "abstract":
+                    string abstractfileToRead = projectPath + @"\KeyWords\AbstractKeywordInfo.json";
+                    return abstractfileToRead;
+                case "async":
+                    string asyncfileToRead = projectPath + @"\KeyWords\AsyncKeywordInfo.json";
+                    return asyncfileToRead;
+                case "const":
+                    string constfileToRead = projectPath + @"\KeyWords\ConstKeywordInfo.json";
+                    return constfileToRead;
+                case "event":
+                    string eventfileToRead = projectPath + @"\KeyWords\EventKeywordInfo.json";
+                    return eventfileToRead;
+                case "extern":
+                    string externfileToRead = projectPath + @"\KeyWords\ExternKeywordInfo.json";
+                    return externfileToRead;
+                case "new":
+                    string newfileToRead = projectPath + @"\KeyWords\NewKeywordInfo.json";
+                    return newfileToRead;
+                case "override":
+                    string overridefileToRead = projectPath + @"\KeyWords\OverrideKeuwordInfo.json";
+                    return overridefileToRead;
+                case "partial":
+                    string partialfileToRead = projectPath + @"\KeyWords\PartialKeywordInfo.json";
+                    return partialfileToRead;
+                case "readonly":
+                    string readonlyfileToRead = projectPath + @"\KeyWords\ReadonlyKeywordInfo.json";
+                    return readonlyfileToRead;
+                case "sealed":
+                    string sealedfileToRead = projectPath + @"\KeyWords\SealedKeywordInfo.json";
+                    return sealedfileToRead;
+                case "static":
+                    string staticfileToRead = projectPath + @"\KeyWords\StaticKeywordInfo.json";
+                    return staticfileToRead;
+                case "unsafe":
+                    string unsafefileToRead = projectPath + @"\KeyWords\UnsafeKeywordInfo.json";
+                    return unsafefileToRead;
+                case "virtual":
+                    string virtualfileToRead = projectPath + @"\KeyWords\VirtualKeywordInfo.json";
+                    return virtualfileToRead;
+                case "volatile":
+                    string volatilefileToRead = projectPath + @"\KeyWords\VolatileKeywordInfo.json";
+                    return volatilefileToRead;
+                default:
+                    return null;
+            }
+        }
+    }
+}
